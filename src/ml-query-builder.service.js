a5b3892c1ed51cc02bd6f87ebeeb3c95160feeb8
Geert Josten <geert.josten@marklogic.com>
2015-07-30 15:48:26 +0200

    Fixed #1: improved query building support

--- src/ml-query-builder.service.js
+++ src/ml-query-builder.service.js
@@ -4,9 +4,64 @@
   angular.module('ml.common')
     .factory('MLQueryBuilder', MLQueryBuilder);
 
+  /*
+   * Structured Query Syntax Reference: http://docs.marklogic.com/guide/search-dev/structured-query#id_59265
+   */
   function MLQueryBuilder() {
+    
+    // [GJo] Allow exposing these under DEPRECATED names as well
+    
+    function qtext(text) {
+      return {
+        'qtext': text
+      };
+    }
+
+    function rangeConstraint(name, operator, values, options) {
+      if (!values && !options) {
+        values = operator;
+        operator = undefined;
+      }
+      operator = operator || 'EQ';
+      values = asArray.apply(null, [values]);
+      options = asArray.apply(null, [options]);
+      return {
+        'range-constraint-query': {
+          'constraint-name': name,
+          'range-operator': operator,
+          'value': values,
+          'range-option': options
+        }
+      };
+    }
+    
+    function customConstraint(name, terms) {
+      terms = asArray.apply(null, [terms]);
+      return {
+        'custom-constraint-query': {
+          'constraint-name': name,
+          'text': terms
+        }
+      };
+    }
+    
+    function propertiesFragment(query) {
+      return { 'properties-fragment-query': query };
+    }
+    
+    function operatorState(name, state) {
+      return {
+        'operator-state': {
+          'operator-name': name,
+          'state-name': state
+        }
+      };
+    }
+    
     return {
 
+      // [GJo] methods in order of the syntax ref
+      
       query: function query() {
         var args = asArray.apply(null, arguments);
         return {
@@ -16,17 +71,27 @@
         };
       },
 
-      text: function text(qtext) {
+      // [GJo] undocumented?
+      qtext: qtext,
+
+      term: function term(text, weight) {
+        text = asArray.apply(null, [text]);
+        weight = weight || 1.0;
         return {
-          'qtext': qtext
+          'term-query': {
+            'text': text,
+            'weight': weight
+          }
         };
       },
 
       and: function and() {
         var args = asArray.apply(null, arguments);
+        var ordered = false;
         return {
           'and-query': {
-            'queries': args
+            'queries': args,
+            'ordered': ordered
           }
         };
       },
@@ -40,83 +105,241 @@
         };
       },
 
-      not: function properties(query) {
+      // TODO: and-not-query
+
+      not: function not(query) {
         return {
           'not-query': query
         };
       },
-
-      document: function document() {
-        var args = asArray.apply(null, arguments);
-        return {
-          'document-query': {
-            'uri': args
-          }
-        };
-      },
-
-      range: function range(name, values) {
-        values = asArray.apply(null, [values]);
-        return {
-          'range-constraint-query': {
-            'constraint-name': name,
-            'value': values
-          }
-        };
-      },
-
-      collection: function collection(name, values) {
-        values = asArray.apply(null, [values]);
-        return {
-          'collection-constraint-query': {
-            'constraint-name': name,
-            'uri': values
-          }
-        };
-      },
-
-      custom: function custom(name, values) {
-        values = asArray.apply(null, [values]);
-        return {
-          'custom-constraint-query': {
-            'constraint-name': name,
-            'value': values
-          }
-        };
-      },
-
-      constraint: function constraint(type) {
-        switch(type) {
-          case 'custom':
-            return this.custom;
-          case 'collection':
-            return this.collection;
-          default:
-            return this.range;
-        }
-      },
+      
+      // TODO: not-in-query
+      // TODO: near-query
 
       boost: function boost(matching, boosting) {
+        if (!boosting) {
+          boosting = matching;
+          matching = undefined;
+        }
         return {
           'boost-query': {
-            'matching-query': matching,
+            'matching-query': matching || this.and(),
             'boosting-query': boosting
           }
         };
       },
 
-      properties: function properties(query) {
-        return { 'properties-query': query };
-      },
+      propertiesFragment: propertiesFragment,
 
-      operator: function operator(name, stateName) {
+      directory: function directory() {
+        var uris = asArray.apply(null, arguments);
+        var infinite = true;
         return {
-          'operator-state': {
-            'operator-name': name,
-            'state-name': stateName
+          'directory-query': {
+            'uri': uris,
+            'infinite': infinite
           }
         };
-      }
+      },
+
+      collection: function collection() {
+        var uris = asArray.apply(null, arguments);
+        return {
+          'collection-query': {
+            'uri': uris
+          }
+        };
+      },
+
+      // TODO: container-query
+
+      document: function document() {
+        var uris = asArray.apply(null, arguments);
+        return {
+          'document-query': {
+            'uri': uris
+          }
+        };
+      },
+
+      documentFragment: function documentFragment(query) {
+        return { 'document-fragment-query': query };
+      },
+
+      locksFragment: function locksFragment(query) {
+        return { 'locks-fragment-query': query };
+      },
+      
+      // TODO: range-query
+      // TODO: value-query
+      // TODO: word-query
+      // TODO: geo-elem-query
+      // TODO: geo-elem-pair-query
+      // TODO: geo-attr-pair-query
+      // TODO: geo-path-query
+      // TODO: geo-json-property-query
+      // TODO: geo-json-property-pair-query
+      
+      rangeConstraint: rangeConstraint,
+
+      valueConstraint: function valueConstraint(name, text, weight) {
+        weight = weight || 1.0;
+        var type;
+        if (text === null) {
+          type = 'null';
+          text = asArray.apply(null, [text]);
+        } else {
+          text = asArray.apply(null, [text]);
+          type = typeof text[0];
+          type = ((type === 'string') && 'text') || type;
+        }
+        var obj = {
+          'value-constraint-query': {
+            'constraint-name': name,
+            'weight': weight
+          }
+        };
+        obj[type] = text;
+        return obj;
+      },
+
+      wordConstraint: function wordConstraint(name, text, weight) {
+        text = asArray.apply(null, [text]);
+        weight = weight || 1.0;
+        return {
+          'word-constraint-query': {
+            'constraint-name': name,
+            'text': text,
+            'weight': weight
+          }
+        };
+      },
+
+      collectionConstraint: function collectionConstraint(name, uris) {
+        uris = asArray.apply(null, [uris]);
+        return {
+          'collection-constraint-query': {
+            'constraint-name': name,
+            'uri': uris
+          }
+        };
+      },
+
+      // TODO: container-constraint-query
+      // TODO: element-constraint-query
+      // TODO: properties-constraint-query
+      
+      customConstraint: customConstraint,
+
+      customGeospatialConstraint: function customGeospatialConstraint(name, annotation, shapes) {
+        if (!shapes) {
+          shapes = annotation;
+          annotation = undefined;
+        }
+        
+        shapes = asArray.apply(null, [shapes]);
+        
+        var points = [];
+        var boxes = [];
+        var circles = [];
+        var polygons = [];
+        
+        for (var i = 0; i < shapes.length; i++) {
+          var shape = shapes[i];
+          
+          if (shape.latitude) {
+            points.push(shape);
+          } else if (shape.south) {
+            boxes.push(shape);
+          } else if (shape.radius) {
+            circles.push(shape);
+          } else if (shape.point) {
+            polygons.push(shape);
+          }
+        }
+        
+        return {
+          'custom-constraint-query': {
+            'constraint-name': name,
+            'annotation': annotation,
+            'point': points,
+            'box': boxes,
+            'circle': circles,
+            'polygon': polygons,
+          }
+        };
+      },
+
+      geospatialConstraint: function geospatialConstraint(name, shapes) {
+        shapes = asArray.apply(null, [shapes]);
+        
+        var points = [];
+        var boxes = [];
+        var circles = [];
+        var polygons = [];
+        
+        for (var i = 0; i < shapes.length; i++) {
+          var shape = shapes[i];
+          
+          if (shape.latitude) {
+            points.push(shape);
+          } else if (shape.south) {
+            boxes.push(shape);
+          } else if (shape.radius) {
+            circles.push(shape);
+          } else if (shape.point) {
+            polygons.push(shape);
+          }
+        }
+        
+        return {
+          'geospatial-constraint-query': {
+            'constraint-name': name,
+            'point': points,
+            'box': boxes,
+            'circle': circles,
+            'polygon': polygons,
+          }
+        };
+      },
+
+      // TODO: lsqt-query
+      // TODO: period-compare-query
+      // TODO: period-range-query
+
+      operatorState: operatorState,
+
+      // Convenience method
+      constraint: function constraint(type) {
+        switch(type) {
+          case 'range':
+            return this.rangeConstraint;
+          case 'value':
+            return this.valueConstraint;
+          case 'word':
+            return this.wordConstraint;
+          case 'collection':
+            return this.collectionConstraint;
+          case 'custom':
+            return this.customConstraint;
+          case 'custom-geospatial':
+            return this.customGeospatialConstraint;
+          case 'geospatial':
+            return this.geospatialConstraint;
+          default:
+            return this.rangeConstraint;
+        }
+      },
+      
+      // DEPRECATED
+      
+      text: qtext,
+      range: rangeConstraint,
+      // [GJo] conflicts with collection-query!
+      //collection: collectionConstraint
+      custom: customConstraint,
+      properties: propertiesFragment,
+      operator: operatorState
 
     };
 
--- test/spec/ml-query-builder.service.js
+++ test/spec/ml-query-builder.service.js
@@ -13,21 +13,21 @@ describe('MLQueryBuilder', function () {
 
   it('builds a query', function() {
     var query = qb.query();
-    expect(query.query).toBeDefined;
+    expect(query.query).toBeDefined();
     expect(query.query.queries.length).toEqual(0);
-    query = qb.query(qb.and())
+    query = qb.query(qb.and());
     expect(query.query.queries.length).toEqual(1);
   });
 
-  it('builds a text query', function() {
+  it('builds a qtext query', function() {
     var query = qb.text('blah');
-    expect(query.qtext).toBeDefined;
+    expect(query.qtext).toBeDefined();
     expect(query.qtext).toEqual('blah');
   });
 
   it('builds an and-query with one sub-query', function() {
     var query = qb.and();
-    expect(query['and-query']).toBeDefined;
+    expect(query['and-query']).toBeDefined();
     expect(query['and-query'].queries.length).toEqual(0);
 
     query = qb.and( qb.text('blah'));
@@ -35,7 +35,7 @@ describe('MLQueryBuilder', function () {
   });
 
   it('builds an and-query with multiple sub-query', function() {
-    var query = query = qb.and( qb.text('blah'), qb.text('blue') );
+    var query = qb.and( qb.text('blah'), qb.text('blue') );
 
     expect(query['and-query'].queries.length).toEqual(2);
   });
@@ -62,7 +62,7 @@ describe('MLQueryBuilder', function () {
   });
 
   // Document query
-  it('builds a document query with one document', function() {
+  it('builds a document-query with one document', function() {
     var query = qb.document('uri');
 
     expect(query['document-query']).toBeDefined();
@@ -70,7 +70,7 @@ describe('MLQueryBuilder', function () {
     expect(query['document-query'].uri[0]).toEqual('uri');
   });
 
-  it('builds a document query with multiple documents', function() {
+  it('builds a document-query with multiple documents', function() {
     var query = qb.document(['uri1', 'uri2']);
 
     expect(query['document-query']).toBeDefined();
@@ -79,27 +79,27 @@ describe('MLQueryBuilder', function () {
     expect(query['document-query'].uri[1]).toEqual('uri2');
   });
 
-  it('builds a range-query with one value', function() {
+  it('builds a range-constraint-query with one value', function() {
     var query = qb.range('test', 'value');
 
     expect(query['range-constraint-query']).toBeDefined();
     expect(query['range-constraint-query']['constraint-name']).toEqual('test');
-    expect(query['range-constraint-query']['value'].length).toEqual(1);
-    expect(query['range-constraint-query']['value'][0]).toEqual('value');
+    expect(query['range-constraint-query'].value.length).toEqual(1);
+    expect(query['range-constraint-query'].value[0]).toEqual('value');
   });
 
-  it('builds a range-query with multiple values', function() {
+  it('builds a range-constraint-query with multiple values', function() {
     var query = qb.range('test', ['value1', 'value2']);
 
     expect(query['range-constraint-query']).toBeDefined();
     expect(query['range-constraint-query']['constraint-name']).toEqual('test');
-    expect(query['range-constraint-query']['value'].length).toEqual(2);
-    expect(query['range-constraint-query']['value'][0]).toEqual('value1');
-    expect(query['range-constraint-query']['value'][1]).toEqual('value2');
+    expect(query['range-constraint-query'].value.length).toEqual(2);
+    expect(query['range-constraint-query'].value[0]).toEqual('value1');
+    expect(query['range-constraint-query'].value[1]).toEqual('value2');
   });
 
-  it('builds a collection-query with one collection', function() {
-    var query = qb.collection('name', 'uri');
+  it('builds a collection-constraint-query with one collection', function() {
+    var query = qb.collectionConstraint('name', 'uri');
 
     expect(query['collection-constraint-query']).toBeDefined();
     expect(query['collection-constraint-query']['constraint-name']).toEqual('name');
@@ -107,8 +107,8 @@ describe('MLQueryBuilder', function () {
     expect(query['collection-constraint-query'].uri[0]).toEqual('uri');
   });
 
-  it('builds a collection-query with multiple collections', function() {
-    var query = qb.collection('name', ['uri1', 'uri2']);
+  it('builds a collection-constraint-query with multiple collections', function() {
+    var query = qb.collectionConstraint('name', ['uri1', 'uri2']);
 
     expect(query['collection-constraint-query']).toBeDefined();
     expect(query['collection-constraint-query']['constraint-name']).toEqual('name');
@@ -117,56 +117,70 @@ describe('MLQueryBuilder', function () {
     expect(query['collection-constraint-query'].uri[1]).toEqual('uri2');
   });
 
-  it('builds a custom-query with one value', function() {
+  it('builds a custom-constraint-query with one value', function() {
     var query = qb.custom('test', 'value');
 
     expect(query['custom-constraint-query']).toBeDefined();
     expect(query['custom-constraint-query']['constraint-name']).toEqual('test');
-    expect(query['custom-constraint-query']['value'].length).toEqual(1);
-    expect(query['custom-constraint-query']['value'][0]).toEqual('value');
+    expect(query['custom-constraint-query'].text.length).toEqual(1);
+    expect(query['custom-constraint-query'].text[0]).toEqual('value');
   });
 
-  it('builds a custom-query with multiple values', function() {
+  it('builds a custom-constraint-query with multiple values', function() {
     var query = qb.custom('test', ['value1', 'value2']);
 
     expect(query['custom-constraint-query']).toBeDefined();
     expect(query['custom-constraint-query']['constraint-name']).toEqual('test');
-    expect(query['custom-constraint-query']['value'].length).toEqual(2);
-    expect(query['custom-constraint-query']['value'][0]).toEqual('value1');
-    expect(query['custom-constraint-query']['value'][1]).toEqual('value2');
+    expect(query['custom-constraint-query'].text.length).toEqual(2);
+    expect(query['custom-constraint-query'].text[0]).toEqual('value1');
+    expect(query['custom-constraint-query'].text[1]).toEqual('value2');
   });
 
   it('chooses a constraint query by type', function() {
     var constraint;
 
-    constraint = qb.constraint(null)
-    expect(constraint('name', 'value')).toEqual(qb.range('name', 'value'))
+    constraint = qb.constraint(null);
+    expect(constraint('name', 'value')).toEqual(qb.rangeConstraint('name', 'value'));
 
-    constraint = qb.constraint('collection')
-    expect(constraint('name', 'value')).toEqual(qb.collection('name', 'value'))
+    constraint = qb.constraint('range');
+    expect(constraint('name', 'value')).toEqual(qb.rangeConstraint('name', 'value'));
 
-    constraint = qb.constraint('custom')
-    expect(constraint('name', 'value')).toEqual(qb.custom('name', 'value'))
+    constraint = qb.constraint('value');
+    expect(constraint('name', 'value')).toEqual(qb.valueConstraint('name', 'value'));
+
+    constraint = qb.constraint('word');
+    expect(constraint('name', 'value')).toEqual(qb.wordConstraint('name', 'value'));
+
+    constraint = qb.constraint('collection');
+    expect(constraint('name', 'value')).toEqual(qb.collectionConstraint('name', 'value'));
+
+    constraint = qb.constraint('custom');
+    expect(constraint('name', 'value')).toEqual(qb.customConstraint('name', 'value'));
+
+    constraint = qb.constraint('custom-geospatial');
+    expect(constraint('name', 'value')).toEqual(qb.customGeospatialConstraint('name', 'value'));
+
+    constraint = qb.constraint('geospatial');
+    expect(constraint('name', 'value')).toEqual(qb.geospatialConstraint('name', 'value'));
   });
 
-  it('builds a boost query', function() {
+  it('builds a boost-query', function() {
     var query = qb.boost( qb.and(), qb.text('blah') );
 
     expect(query['boost-query']).toBeDefined();
     expect(query['boost-query']['matching-query']).toBeDefined();
     expect(query['boost-query']['matching-query']).toEqual( qb.and() );
-
     expect(query['boost-query']['boosting-query'].qtext).toEqual('blah');
   });
 
-  it('builds a properties query', function() {
+  it('builds a properties-fragment-query', function() {
     var query = qb.properties( qb.and() );
 
-    expect(query['properties-query']).toBeDefined();
-    expect(query['properties-query']).toEqual( qb.and() );
+    expect(query['properties-fragment-query']).toBeDefined();
+    expect(query['properties-fragment-query']).toEqual( qb.and() );
   });
 
-  it('builds an operator query', function() {
+  it('builds an operator-state', function() {
     var query = qb.operator('sort', 'date');
 
     expect(query['operator-state']).toBeDefined();
